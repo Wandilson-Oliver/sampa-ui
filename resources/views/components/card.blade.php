0b1fbdd959d45c320b0a3b2e14e953bf4fb5b44d
Wandilson Oliver <wandilson.oliver@gmail.com>
2025-12-26 20:40:27 -0400

    Ajustes nos componentes Card e Link

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,34 @@
-<div
-    {{ $attributes->class([
-        'rounded-lg border bg-white dark:bg-neutral-900',
-    ]) }}
->
-    {{ $slot }}
+@props([
+    'title' => null,
+    'subtitle' => null,
+])
+
+<div {{ $attributes->class(['rounded-lg border bg-white dark:bg-neutral-900 shadow p-4']) }}>
+    {{-- Header --}}
+    @if($title || $subtitle || isset($header))
+        <div class="mb-4">
+            @isset($header)
+                {{ $header }}
+            @else
+                @if($title)
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{ $title }}</h3>
+                @endif
+                @if($subtitle)
+                    <p class="text-sm text-gray-500 dark:text-gray-400">{{ $subtitle }}</p>
+                @endif
+            @endif
+        </div>
+    @endif
+
+    {{-- Body --}}
+    <div class="mb-4">
+        {{ $slot }}
+    </div>
+
+    {{-- Footer --}}
+    @isset($footer)
+        <div class="mt-4 border-t pt-3 flex space-x-2">
+            {{ $footer }}
+        </div>
+    @endisset
 </div>
--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -7,7 +7,7 @@
     href="{{ $href }}"
     @if($target) target="{{ $target }}" @endif
     {{ $attributes->class([
-        'text-slate-600 underline',
+        'text-slate-800 hover:underline',
     ]) }}
 >
     {{ $slot }}
