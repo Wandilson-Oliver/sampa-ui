14ed5e2720de9ea0f1caa8048c02ca8bde68cc7d
Wandilson Oliver <wandilson.oliver@gmail.com>
2025-12-28 14:01:22 -0400

    Ajustes nos Botoes

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,10 +4,10 @@
 
 @php
 $base = 'inline-flex items-center justify-center gap-2
-        px-4 py-2 rounded-md
-        font-medium text-sm
+        px-5 py-2.5 rounded-md
+        font-light text-sm
         transition-colors duration-200
-        focus:outline-none focus:ring-2 focus:ring-offset-2
+        focus:outline-none focus:ring-2 focus:ring-offset-2 cursor-pointer
         focus:ring-offset-white dark:focus:ring-offset-neutral-900
         disabled:opacity-50 disabled:pointer-events-none';
 
