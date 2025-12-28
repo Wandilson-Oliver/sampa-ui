35103061fe3e2a38dcf9e62203836a95d447ecb1
Wandilson Oliver <wandilson.oliver@gmail.com>
2025-12-28 13:56:21 -0400

    Ajustes nos Botoes

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -13,25 +13,25 @@ $base = 'inline-flex items-center justify-center gap-2
 
 $variants = [
     'default'   => 'bg-slate-50 text-neutral-900 hover:bg-neutral-300
-                    dark:bg-neutral-700 dark:text-white dark:hover:bg-neutral-600',
+                    dark:bg-slate-50 dark:text-white dark:hover:bg-neutral-200',
 
-    'primary'   => 'bg-blue-400 text-white hover:bg-blue-500
-                    dark:bg-blue-500 dark:hover:bg-blue-600',
+    'primary'   => 'bg-blue-500 text-white hover:bg-blue-600
+                    dark:bg-blue-600 dark:hover:bg-blue-600',
 
-    'secondary' => 'bg-fuchsia-400 text-white hover:bg-fuchsia-500
-                    dark:bg-fuchsia-500 dark:hover:bg-fuchsia-600',
+    'secondary' => 'bg-pink-500 text-white hover:bg-pink-600
+                    dark:bg-pink-600 dark:hover:bg-pink-600',
 
-    'info'      => 'bg-sky-400 text-white hover:bg-sky-500
-                    dark:bg-sky-500 dark:hover:bg-sky-600',
+    'info'      => 'bg-sky-500 text-white hover:bg-sky-600
+                    dark:bg-sky-600 dark:hover:bg-sky-600',
 
-    'success'   => 'bg-emerald-400 text-white hover:bg-emerald-500
-                    dark:bg-emerald-500 dark:hover:bg-emerald-600',
+    'success'   => 'bg-teal-500 text-white hover:bg-teal-600
+                    dark:bg-teal-600 dark:hover:bg-teal-600',
 
-    'warning'   => 'bg-amber-400 text-white hover:bg-amber-500
-                    dark:bg-amber-500 dark:hover:bg-amber-600',
+    'warning'   => 'bg-orange-500 text-white hover:bg-orange-600
+                    dark:bg-orange-600 dark:hover:bg-orange-600',
 
-    'error'     => 'bg-red-400 text-white hover:bg-red-500
-                    dark:bg-red-500 dark:hover:bg-red-600',
+    'error'     => 'bg-red-500 text-white hover:bg-red-600
+                    dark:bg-red-600 dark:hover:bg-red-600',
 ];
 @endphp
 
